8c2c83208383e6112e4bbe9f5210ad99edc7b339
Evan Jacobs <ejacobs@springbot.com>
2016-05-19 19:15:23 -0400

    Remove incorrect DI from constructors

--- Helper/Data.php
+++ Helper/Data.php
@@ -2,9 +2,8 @@
 
 namespace Springbot\Queue\Helper;
 
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Helper\AbstractHelper;
-use Magento\Framework\App\Helper\Context;
+
 
 /**
  * Class Data
@@ -14,17 +13,4 @@ use Magento\Framework\App\Helper\Context;
 class Data extends AbstractHelper
 {
 
-    protected $_scopeConfig;
-
-    /**
-     * Data constructor.
-     *
-     * @param Context $context
-     * @param ScopeConfigInterface $scopeConfig
-     */
-    public function __construct(Context $context, ScopeConfigInterface $scopeConfig)
-    {
-        $this->_scopeConfig = $scopeConfig;
-        parent::__construct($context);
-    }
 }
