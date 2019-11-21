821965ecb85e4bae9ef3852d488e6c14e0279428
Joey Kimsey <JoeyK4816@users.noreply.github.com>
2019-11-21 09:56:32 -0500

    Update Job.php
    This should prevent the job from freezing up the queue in cases where the arguments are insufficient to call the method.

--- Model/Job.php
+++ Model/Job.php
@@ -36,6 +36,10 @@ class Job extends AbstractModel
         if (class_exists($class)) {
             $object = $this->_objectManager->get($class);
             if (method_exists($object, $method)) {
+                $refl = new ReflectionMethod(get_class($object), $method);
+                if ($refl->getNumberOfRequiredParameters() > count($args)) {
+                    throw new Exception("Too few arguments to call {$class} {method}");
+                }
                 return call_user_func_array([$object, $method], $args);
             } else {
                 throw new Exception("Method {$method} does not exist in class {$class}");
